96ee39166d795ec6d58e5908a5baba45d0c8842d
MarynaChicherina <marinachicherina.ua@gmail.com>
2023-03-14 15:00:45 +0200

    Add task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+//рандомна зміна кольору
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  } 
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+//зміна кольору background
+function changeBackground() {
+    const bodyColor = getRandomHexColor();
+    body.style.backgroundColor = bodyColor;
+}
+
+let intervalColorChange = undefined;
+
+function onStartBtnClick() {
+    intervalColorChange = setInterval(() => changeBackground(), 1000);
+    stopBtn.removeAttribute('disabled');
+    startBtn.setAttribute('disabled', true);
+}
+
+function onStopBtnClick() {
+    clearInterval(intervalColorChange);
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', true);
+}
+
+
