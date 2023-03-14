bfd4de13e8e3c25c21bcf69e9b1e16d3036ba06e
MarynaChicherina <113783241+MarynaChicherina@users.noreply.github.com>
2023-03-14 13:01:10 +0000

    Deploying to gh-pages from @ MarynaChicherina/goit-js-hw-09@96ee39166d795ec6d58e5908a5baba45d0c8842d 🚀

---
+++ 01-color-switcher.08020f9c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){o=setInterval((function(){return t="#".concat(Math.floor(16777215*Math.random()).toString(16)),void(r.style.backgroundColor=t);var t}),1e3),e.removeAttribute("disabled"),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));var o=void 0}();
+//# sourceMappingURL=01-color-switcher.08020f9c.js.map
